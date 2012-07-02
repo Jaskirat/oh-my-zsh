7954054a33d6cf1cde73ec7ce3ca68d1f11e968c
Jaskirat <me@jaskirat.info>
2012-07-02 17:13:32 +0530

    Adding some shorthands and disabling default shorthands in git plugin Checking in my zshrc along with this as well.

---
+++ custom/shorthands.zsh
@@ -0,0 +1,26 @@
+# some ls aliases                                                             
+alias ll='ls -l'
+alias lh='ls -lh'
+alias la='ls -A'
+alias l='ls -CF'
+alias ld='ls -l| grep ^d '
+
+#some emacs aliases
+alias sumacs='sudo emacs --no-desktop'
+alias ec='emacsclient --no-wait'
+alias ect='emacsclient -t'
+
+# Git aliases for then times
+# when i don't use magit
+alias ga='git add'
+alias gp='git push'
+alias gu='git pull'
+alias gl='git log'
+alias gs='git status'
+alias gd='git diff'
+alias gc='git commit'
+alias gm='git commit -m'
+alias gma='git commit -am'
+alias gca='git commit --amend'
+alias gb='git branch'
+alias gs='git checkout'
